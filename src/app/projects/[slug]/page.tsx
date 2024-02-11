d1f2d816e0ddbd5f0d52da794919431a5fb51c39
Juan Oscco <juan.osccomori@gmail.com>
2024-02-10 20:35:01 -0500

    creando pagina slug

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { myProjects } from "@/mocks/myProjects.mock"
+import { GitHubLogoIcon, VercelLogoIcon } from "@radix-ui/react-icons";
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Props {
     params: { slug: string }
@@ -13,23 +15,55 @@ export default function Page({ params }: Props) {
     const project = myProjects.find(project => project.slug === slug);
 
     return (
-        <section className='w-11/12 max-w-2xl m-auto'>
+        <section className='w-10/12 max-w-2xl m-auto'>
             <Image
-            src={`https://source.unsplash.com/random/600x300/?programmer`}
-            alt='my-tech-stack'
-            width={400}
-            height={500}
+                src={`https://source.unsplash.com/random/600x300/?programmer`}
+                alt='my-tech-stack'
+                width={400}
+                height={100}
+                className='rounded-lg shadow-md w-full h-auto sm:h-48'
             />
-            <h1>{project?.title}</h1>
-            <p>Slug: {slug}</p>
+
             {project && (
-                <div>
-                    <p>Fecha: {project.date}</p>
-                    <p>Descripción: {project.description}</p>
-                    <p>Tipo: {project.type}</p>
-                    <p>Skills: {project.skills.join(', ')}</p>
-                    <a href={project.link}>Enlace al Repositorio</a>
-                </div>
+                <section className='flex flex-col-reverse sm:flex-row-reverse mt-5'>
+                    <article className='px-5 my-2 sm:block hidden'>
+                        <h1 className='text-xl font-bold '>Skills</h1>
+                        <ul className="list-disc gap-7  sm:gap-0 sm:pl-4 px-6 sm:px-0 sm:py-4 flex justify-start sm:flex-col">
+                            {project.skills.map((skill: string, index: number) => (
+                                <li className='px-1 py-1' key={index}>{skill}</li>
+                            ))}
+                        </ul>
+                    </article>
+
+
+                    <article className=" p-4 rounded-lg ">
+                        <h1 className="text-3xl font-bold mb-2">{project?.title}</h1>
+                        <p className="mb-2">{project?.description}</p>
+                        <p className="mb-2">Date: {project?.date}</p>
+
+                        <article className='sm:hidden block my-5'>
+                            <h1 className='text-xl font-bold '>Skills</h1>
+                            <ul className="list-disc gap-7  sm:gap-0 sm:pl-4 px-6 sm:px-0 sm:py-4 py-3 flex justify-start sm:flex-col">
+                                {project.skills.map((skill: string, index: number) => (
+                                    <li className='px-1 py-1' key={index}>{skill}</li>
+                                ))}
+                            </ul>
+                        </article>
+
+                        <article>
+                            <h1 className='text-xl font-bold my-3'>Links</h1>
+                            <div className='flex gap-2'>
+                                <a href={project.link} target='_blank' rel='noreferrer'>
+                                    <GitHubLogoIcon height={20} width={20} className=' rounded-full ' />
+                                </a>
+                                <a href={project.browser} target='_blank' rel='noreferrer'>
+                                    <VercelLogoIcon height={20} width={20} className=' rounded-full ' />
+                                </a>
+                            </div>
+                        </article>
+                    </article>
+
+                </section>
             )}
         </section>
     )
--- src/mocks/myProjects.mock.ts
+++ src/mocks/myProjects.mock.ts
@@ -1,74 +1,79 @@
 import { Props } from "@/interface/projects.interface";
 
-
 export const myProjects: Props[] = [
-    {
-      title: "Angular Components",
-      slug: "angular-components",
-      date: "2023-01-01",
-      description:
-        "A collection of reusable Angular components for building dynamic web applications.",
-      destacated: false,
-      type: "Angular",
-      link: "https://github.com/jcom-dev/windbnb",
-      skills: ["Angular", "TypeScript", "SASS/SCSS"],
-    },
-    {
-      title: "My Gallery",
-      slug: "my-gallery",
-      date: "2023-02-01",
-      description:
-        "An image gallery application built with Next.js and React for showcasing personal photos and images.",
-      destacated: true,
-      type: "Next.js",
-      link: "https://github.com/jcom-dev/my-gallery",
-      skills: ["Next.js", "TypeScript", "Tailwind CSS"],
-    },
-    {
-      title: "Quiz Country",
-      slug: "quiz-country",
-      description:
-        "A quiz application focused on testing knowledge about countries and their capitals.",
-      date: "2023-06-30",
-      destacated: false,
-      link: "https://github.com/jcom-dev/quiz-country",
-      type: "Next.js",
-      skills: ["Next.js", "React", "Tailwind CSS"],
-    },
-    {
-      title: "Random quote Generator",
-      slug: "random-quote-generator",
-      date: "2023-03-01",
-      description:
-        "A tool for generating random quotes and phrases sourced from the quote garden API.",
-      type: "Next.js",
-      link: "https://github.com/jcom-dev/random-quote-generator",
-      destacated: true,
-      skills: ["Next.js", "Tailwind CSS", "quote garden API"],
-    },
-    {
-      title: "todo App",
-      slug: "todo-app",
-      date: "2023-04-01",
-      description:
-        "A task management application for creating, organizing, and tracking daily to-do lists and tasks.",
-      destacated: true,
-      type: "Next.js",
-      link: "https://github.com/jcom-dev/to-do-app",
-      skills: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
-    },
-    {
-      title: "windbnb",
-      slug: "windbnb",
-      description: "A project to clone the Airbnb site for Windbnb in Angular.",
-      date: "2023-06-30",
-      link: "https://github.com/jcom-dev/windbnb",
-      destacated: false,
-      type: "Angular",
-      skills: ["Angular", "TypeScript", "SASS/SCSS"],
-    },
-  
-  
+  {
+    title: "Angular Components",
+    slug: "angular-components",
+    date: "2023-01-01",
+    description:
+      "A collection of reusable Angular components for building dynamic web applications.",
+    destacated: false,
+    type: "Angular",
+    link: "https://github.com/jcom-dev/components-angular",
+    browser: "https://jcom-dev.github.io/components-angular/button",
+    skills: ["Angular", "TypeScript", "SASS/SCSS"],
+  },
+  {
+    title: "My Gallery",
+    slug: "my-gallery",
+    date: "2023-02-01",
+    description:
+      "An image gallery application built with Next.js and React for showcasing personal photos and images.",
+    destacated: true,
+    type: "Next.js",
+    link: "https://github.com/jcom-dev/my-gallery",
+    browser: "https://jcom-dev.github.io/my-gallery/",
+    skills: ["Next.js", "TypeScript", "Tailwind CSS"],
+  },
+  {
+    title: "Quiz Country",
+    slug: "quiz-country",
+    description:
+      "A quiz application focused on testing knowledge about countries and their capitals.",
+    date: "2023-06-30",
+    destacated: false,
+    link: "https://github.com/jcom-dev/quiz-country",
+    browser: "https://quiz-country.vercel.app/",
+    type: "Next.js",
+    skills: ["Next.js", "React", "Tailwind CSS"],
+  },
+  {
+    title: "Random quote Generator",
+    slug: "random-quote-generator",
+    date: "2023-03-01",
+    description:
+      "A tool for generating random quotes and phrases sourced from the quote garden API.",
+    type: "Next.js",
+    link: "https://github.com/jcom-dev/random-quote-generator",
+    browser: "https://random-quote-generator-fawn.vercel.app/",
+    destacated: true,
+    skills: ["Next.js", "Tailwind CSS", "quote garden API"],
+  },
+  {
+    title: "todo App",
+    slug: "todo-app",
+    date: "2023-04-01",
+    description:
+      "A task management application for creating, organizing, and tracking daily to-do lists and tasks.",
+    destacated: true,
+    type: "Next.js",
+    link: "https://github.com/jcom-dev/to-do-app",
+    browser: "https://to-do-app-ten-sigma.vercel.app/",
+    skills: ["Next.js", "React", "TypeScript", "Tailwind CSS"],
+  },
+  {
+    title: "windbnb",
+    slug: "windbnb",
+    description: "A project to clone the Airbnb site for Windbnb in Angular.",
+    date: "2023-06-30",
+    link: "https://github.com/jcom-dev/windbnb",
+    browser: "https://jcom-dev.github.io/windbnb/",
+    destacated: false,
+    type: "Angular",
+    skills: ["Angular", "TypeScript", "SASS/SCSS"],
+  },
+
+
 
   // {
   //   title: "Weather App",
